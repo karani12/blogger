717ca34eeb1f409c9442700a417a48cd8bdf7ec0
karani <karanijohn018@gmail.com>
2021-11-01 10:49:12 +0300

    initial

---
+++ blogger-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a3215d0597589ddf030faad6363da5c2'),
+    },
+  },
+});
